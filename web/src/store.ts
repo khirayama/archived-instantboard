33249ee3818592d5b42776976c54020af46a491a
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-06-19 07:14:28 +0900

    Create store file for web client

---
+++ web/src/store.ts
@@ -0,0 +1,8 @@
+import Store from './libs/circuit';
+
+const initialState = {};
+const reducer = (state: any, action: any) => {
+  return state;
+};
+
+export default new Store(initialState, reducer);

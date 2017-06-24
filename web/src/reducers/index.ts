e522c336b5768f33ba4ea9ae682666d6f990abc4
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-06-24 09:39:15 +0900

    Update reducers

--- web/src/reducers/index.ts
+++ web/src/reducers/index.ts
@@ -1,5 +1,21 @@
 const reducer = (state: any, action: any) => {
-  return state;
+  const newState = Object.assign({}, state);
+  if (action) {
+    switch (action.type) {
+      case '__INITIALIZE_MAIN_STORYBOARD': {
+        newState.isAuthenticated = action.isAuthenticated;
+        break;
+      }
+      case '__INITIALIZE_PROFILE_STORYBOARD': {
+        newState.isAuthenticated = action.isAuthenticated;
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+  return newState;
 };
 
 export default reducer;

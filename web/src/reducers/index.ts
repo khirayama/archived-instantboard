6f10ec9304cf8eee5420d5d7545357de8f55f83c
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-06-27 08:05:53 +0900

    Update reducers

--- web/src/reducers/index.ts
+++ web/src/reducers/index.ts
@@ -1,28 +1,39 @@
-const reducer = (state: any, action: any) => {
-  const newState = Object.assign({}, state);
+const reducer = (state: IState, action: IAction) => {
+  const newState: IState = Object.assign({}, state);
+
   if (action) {
     switch (action.type) {
-      case '__INITIALIZE_MAIN_STORYBOARD': {
+      case '__FETCH_INITIAL_DATA': {
         newState.isAuthenticated = action.isAuthenticated;
         newState.user = action.user;
+        newState.tasks = action.tasks;
         break;
       }
-      case '__INITIALIZE_NEW_USER_STORYBOARD': {
+      case '__FAILURE_FETCH_INITIAL_DATA': {
         newState.isAuthenticated = action.isAuthenticated;
-        newState.user = action.user;
+        newState.user = null;
+        newState.tasks = [];
         break;
       }
       case '__CREATE_TOKEN': {
         newState.isAuthenticated = action.isAuthenticated;
         newState.user = action.user;
+
+        newState.tasks = newState.tasks;
         break;
       }
       case '__UPDATE_USER': {
         newState.user = Object.assign({}, state.user, action.user);
+
+        newState.isAuthenticated = newState.isAuthenticated;
+        newState.tasks = newState.tasks;
         break;
       }
       case '__FAILURE_UPDATE_USER': {
-        newState.user.errors.push(action.error);
+        newState.user = action.user;
+
+        newState.isAuthenticated = newState.isAuthenticated;
+        newState.tasks = newState.tasks;
         break;
       }
       default: {

cb170be80ae144b8e47d22e894b4b86425393ff3
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-06-26 09:07:33 +0900

    Create request handlers

---
+++ api/src/handlers/request-handlers.js
@@ -0,0 +1,47 @@
+const {Request} = require('../models');
+
+function indexRequestHandler(req, res) {
+  Request.findAll({
+    where: {userId: req.user.id},
+    order: [['createdAt', 'ASC']],
+  }).then(requests => {
+    res.json(requests);
+  });
+}
+
+function createRequestHandler(req, res) {
+  Request.create({
+    userId: req.user.id,
+    sharedUserId: req.body.sharedUserId,
+    labelId: req.body.labelId,
+  }).then(request => {
+    res.json(request);
+  });
+}
+
+function updateRequestHandler(req, res) {
+  Request.findById(req.params.id).then(request => {
+    if (request.body.status !== req.body.status) {
+      request.update({
+        status: req.body.status,
+      }).then(() => {
+        res.json(label);
+      });
+    }
+  });
+}
+
+function destroyRequestHandler(req, res) {
+  Request.findById(req.params.id).then(request => {
+    request.destroy().then(destroyedRequest => {
+      res.json(destroyedRequest);
+    });
+  });
+}
+
+module.exports = {
+  indexRequestHandler,
+  createRequestHandler,
+  updateRequestHandler,
+  destroyRequestHandler,
+};

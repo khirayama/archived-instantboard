ac49e85e009faf9d520a906975907d72dbdfa5dc
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-06-27 08:38:06 +0900

    Show tasks and labels

--- web/src/storyboards/main-storyboard/index.tsx
+++ web/src/storyboards/main-storyboard/index.tsx
@@ -12,9 +12,15 @@ export default class MainStoryboard extends Container<any, any> {
       <section className="storyboard">
         <h1>MainStoryboard</h1>
         <div>
+          <ul>
+            {this.state.tasks.map((task: any, index: number) => <li key={index}>{task.content}</li>)}
+          </ul>
           <Link href="/tasks/new">New Tasks</Link>
         </div>
         <div>
+          <ul>
+            {this.state.labels.map((label: any, index: number) => <li key={index}>{label.name}</li>)}
+          </ul>
           <Link href="/labels/new">New Labels</Link>
         </div>
       </section>

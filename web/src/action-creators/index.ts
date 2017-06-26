9234a5ace0c402d335387f5d7db9bea3d860a09d
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-06-27 08:04:43 +0900

    Refactor action-creators

--- web/src/action-creators/index.ts
+++ web/src/action-creators/index.ts
@@ -1,9 +1,10 @@
 import {
-  LoginStatus,
+  Task,
   Token,
   User,
 } from '../repositories';
 
+// Utils
 function mapUser(user: any) {
   return {
     id: user.id,
@@ -12,70 +13,86 @@ function mapUser(user: any) {
   };
 }
 
-function initializeMainStoryboard(params: any, args: any, payload: any) {
-  return new Promise((resolve) => {
-    LoginStatus.get(payload.accessToken).then(({status, user}) => {
+function mapTask(task: any) {
+  return {
+    id: task.id,
+    labelId: task.labelId,
+    content: task.content,
+    priority: task.priority,
+    completed: task.completed,
+    errors: [],
+  };
+}
+
+export function fetchInitialData(dispatch: (action: any) => void, options: any) {
+  return new Promise((resolve, reject) => {
+    User.get(options).then((user: any) => {
+      Task.all(options).then((tasks: any) => {
+        const action = {
+          type: '__FETCH_INITIAL_DATA',
+          isAuthenticated: true,
+          user: mapUser(user),
+          tasks: tasks.map((task: any) => mapTask(task)),
+        };
+        dispatch(action);
+        resolve();
+      });
+    }).catch(() => {
       const action = {
-        type: '__INITIALIZE_MAIN_STORYBOARD',
-        isAuthenticated: (status === 'connected'),
-        user: mapUser(user),
+        type: '__FAILURE_FETCH_INITIAL_DATA',
+        isAuthenticated: false,
       };
-      payload.dispatch(action);
-      resolve();
+      dispatch(action);
+      reject();
     });
   });
 }
 
-function initializeNewUserStoryboard(params: any, args: any, payload: any) {
-  return new Promise((resolve) => {
-    LoginStatus.get(payload.accessToken).then(({status, user}) => {
-      const action = {
-        type: '__INITIALIZE_NEW_USER_STORYBOARD',
-        isAuthenticated: (status === 'connected'),
-        user: mapUser(user),
-      };
-      payload.dispatch(action);
-      resolve();
-    });
-  });
-}
-
-function createToken(params: {provider: string; uid: string; }, payload: any) {
+// Token
+export function createToken(dispatch: (action: any) => void, params: {provider: string; uid: string; }) {
   return new Promise((resolve) => {
     Token.create(params).then(({accessToken}) => {
-      User.get(accessToken).then((user) => {
-        payload.dispatch({
+      User.get({accessToken}).then((user: any) => {
+        const action = {
           type: '__CREATE_TOKEN',
           isAuthenticated: true,
           user: mapUser(user),
-        });
+        };
+        dispatch(action);
         resolve({accessToken, user});
       });
     });
   });
 }
 
-function updateUser(params: {username: string; }, payload: any) {
+// User
+export function updateUser(dispatch: (action: any) => void, params: {username: string; }, options: any) {
   return new Promise((resolve, reject) => {
-    User.update(payload.accessToken, params).then((res) => {
-      payload.dispatch({
+    User.update(params, options).then((user) => {
+      const action = {
         type: '__UPDATE_USER',
-        user: Object.assign({errors: []}, params),
-      });
+        user: mapUser(user),
+      };
+      dispatch(action);
       resolve();
     }).catch(() => {
-      payload.dispatch({
+      const action = {
         type: '__FAILURE_UPDATE_USER',
-        error: 'Already existed',
-      });
+        user: mapUser(Object.assign({}, params, {errors: ['Already existed']})),
+      };
+      dispatch(action);
       reject();
     });
   });
 }
 
-export  {
-  initializeMainStoryboard,
-  initializeNewUserStoryboard,
-  createToken,
-  updateUser,
-};
+// Task
+export function createTask(
+  dispatch: (action: any) => void,
+  params: {content: string; labelId: number; },
+  options: any,
+) {
+  return new Promise((resolve, reject) => {
+    Task.create(params, options);
+  });
+}

31ad82ff9ea7858345700e5ef35f1af5070e8b79
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-07-04 07:55:08 +0900

    Change id schema of migration user create file

--- api/migrations/files/20170414003619-create-user.js
+++ api/migrations/files/20170414003619-create-user.js
@@ -2,7 +2,7 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('users', {
       id: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.INTEGER(255),
         primaryKey: true,
         autoIncrement: true,
         allowNull: false,

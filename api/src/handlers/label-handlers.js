a61f43dcf9f4c6347f8365b4e116cffc8380bf9f
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-07-04 07:56:18 +0900

    Move business logic of label to model

--- api/src/handlers/label-handlers.js
+++ api/src/handlers/label-handlers.js
@@ -1,286 +1,25 @@
-const {Label, Task, Request, LabelStatus, User} = require('../models');
-
-function _getMembers(labelId, requests, users) {
-  const members = [];
-  for (let j = 0; j < requests.length; j++) {
-    const request = requests[j];
-    if (labelId === request.labelId) {
-      for (let k = 0; k < users.length; k++) {
-        const user = users[k];
-        if (user.id === request.memberId || user.id === request.userId) {
-          members.push({
-            id: user.id,
-            username: user.username,
-            requestStatus: request.status,
-          });
-        }
-      }
-    }
-  }
-  return members;
-}
-
-function _indexLabel(userId) {
-  return new Promise(resolve => {
-    LabelStatus.findAll({
-      where: {userId},
-      order: [['priority', 'ASC']],
-    }).then(labelStatuses => {
-      const labelIds = labelStatuses.map(labelStatus => labelStatus.labelId);
-      Promise.all([
-        Label.findAll({
-          where: {id: labelIds},
-        }),
-        Request.findAll({
-          where: {labelId: labelIds},
-        }),
-      ]).then(values => {
-        const labels = values[0];
-        const requests = values[1];
-
-        const userIds = requests.map(request => request.memberId).filter(memberId => (memberId !== userId));
-        User.findAll({
-          where: {id: userIds},
-        }).then(users => {
-          const labels_ = labelStatuses.map(labelStatus => {
-            let newLabel = {};
-            for (let i = 0; i < labels.length; i++) {
-              const label = labels[i];
-              if (label.id === labelStatus.labelId) {
-                newLabel = {
-                  id: label.id,
-                  name: label.name,
-                  priority: labelStatus.priority,
-                  visibled: labelStatus.visibled,
-                  createdAt: label.createdAt,
-                  updatedAt: label.updatedAt,
-                  members: _getMembers(label.id, requests, users),
-                };
-              }
-            }
-            return newLabel;
-          });
-          resolve(labels_);
-        });
-      });
-    });
-  });
-}
-
-function _createLabel(userId, labelName, userNames = []) {
-  return new Promise((resolve, reject) => {
-    Promise.all([
-      User.findAll({
-        where: {username: userNames},
-      }),
-      Label.create({
-        userId,
-        name: labelName,
-      }),
-      LabelStatus.count({
-        where: {userId},
-      }),
-    ]).then(values => {
-      const users = values[0];
-      const label = values[1];
-      const count = values[2];
-
-      if (users.length !== userNames.length) {
-        reject(new Error('Include not existed user'));
-        return;
-      }
-
-      LabelStatus.create({
-        userId,
-        labelId: label.id,
-        priority: count,
-        visibled: true,
-      }).then(() => {
-        _showLabel(userId, label.id).then(createdLabel => {
-          resolve(createdLabel);
-        });
-      });
-    });
-  });
-}
-
-function _showLabel(userId, labelId) {
-  return new Promise(resolve => {
-    Promise.all([
-      Label.findById(labelId),
-      LabelStatus.findOne({
-        where: {userId, labelId},
-      }),
-    ]).then(values => {
-      const label = values[0];
-      const labelStatus = values[1];
-
-      Request.findAll({
-        where: {labelId: label.id},
-      }).then(requests => {
-        const userIds = requests.map(request => request.memberId).filter(memberId => (memberId !== userId));
-
-        User.findAll({
-          where: {id: userIds},
-        }).then(users => {
-          const newLabel = {
-            id: label.id,
-            name: label.name,
-            priority: labelStatus.priority,
-            visibled: labelStatus.visibled,
-            createdAt: labelStatus.createdAt,
-            updatedAt: labelStatus.updatedAt,
-            members: _getMembers(label.id, requests, users),
-          };
-          resolve(newLabel);
-        });
-      });
-    });
-  });
-}
-
-function _updateLabel(userId, labelId, newLabel = {}) {
-  return new Promise(resolve => {
-    Promise.all([
-      Label.findById(labelId),
-      LabelStatus.findOne({
-        where: {userId, labelId},
-      }),
-    ]).then(values => {
-      const label = values[0];
-      const labelStatus = values[1];
-
-      Promise.all([
-        label.update({name: newLabel.name || label.name}),
-        labelStatus.update({
-          priority: (newLabel.priority !== undefined && newLabel.priority !== labelStatus.priority) ? newLabel.priority : labelStatus.priority,
-          visibled: (newLabel.visibled !== undefined && newLabel.visibled !== labelStatus.visibled) ? newLabel.visibled : labelStatus.visibled,
-        }),
-      ]).then(() => {
-        _showLabel(userId, label.id).then(updatedLabel => {
-          resolve(updatedLabel);
-        });
-      });
-    });
-  });
-}
-
-function _destroyLabel(userId, labelId) {
-  return new Promise(resolve => {
-    Promise.all([
-      _showLabel(userId, labelId),
-      Label.findById(labelId),
-    ]).then(values => {
-      const destroyedLabel = values[0];
-      const label = values[1];
-
-      // Remove LabelStatus, Task, Request
-      Promise.all([
-        new Promise(resolve_ => {
-          LabelStatus.findAll({
-            where: {labelId: label.id},
-          }).then(labelStatuses => {
-            let count = 0;
-
-            labelStatuses.forEach(labelStatus => {
-              // Decrement
-              LabelStatus.findAll({
-                where: {
-                  userId: labelStatus.userId,
-                  priority: {
-                    $gt: labelStatus.priority,
-                  },
-                },
-              }).then(labelStatuses => {
-                labelStatuses.forEach(labelStatus_ => labelStatus_.increment({priority: -1}));
-              });
-
-              labelStatus.destroy().then(() => {
-                count += 1;
-                if (count === labelStatuses.length) {
-                  resolve_();
-                }
-              });
-            });
-          });
-        }),
-        Task.destroy({
-          where: {labelId: label.id},
-        }),
-        Request.destroy({
-          where: {labelId: label.id},
-        }),
-      ]).then(() => {
-        label.destroy().then(() => {
-          resolve(destroyedLabel);
-        });
-      });
-    });
-  });
-}
-
-function _sortLabel(userId, labelId, priority) {
-  return new Promise(resolve => {
-    LabelStatus.findOne({
-      where: {userId, labelId},
-    }).then(labelStatus => {
-      if (labelStatus.priority < priority) {
-        LabelStatus.findAll({
-          where: {
-            userId,
-            priority: {
-              $gt: labelStatus.priority,
-              $lte: priority,
-            },
-          },
-        }).then(labelStatuses => {
-          labelStatuses.forEach(labelStatus_ => {
-            labelStatus_.update({priority: labelStatus_.priority - 1});
-          });
-          labelStatus.update({priority}).then(() => {
-            _indexLabel(userId).then(labels => {
-              resolve(labels);
-            });
-          });
-        });
-      } else if (labelStatus.priority > priority) {
-        LabelStatus.findAll({
-          where: {
-            userId,
-            priority: {
-              $gte: priority,
-              $lt: labelStatus.priority,
-            },
-          },
-        }).then(labelStatuses => {
-          labelStatuses.forEach(labelStatus_ => {
-            labelStatus_.update({priority: labelStatus_.priority + 1});
-          });
-          labelStatus.update({priority}).then(() => {
-            _indexLabel(userId).then(labels => {
-              resolve(labels);
-            });
-          });
-        });
-      }
-    });
-  });
-}
+const {Label} = require('../models');
 
 function indexLabelHandler(req, res) {
-  _indexLabel(req.user.id).then(labels => {
+  Label.findAllFromStatus({
+    where: {userId: req.user.id},
+    order: [['priority', 'ASC']],
+  }).then(labels => {
     res.json(labels);
   });
 }
 
 function showLabelHandler(req, res) {
-  _showLabel(req.user.id, req.params.id).then(label => {
+  Label.findByIdAndUser(req.params.id, req.user.id).then(label => {
     res.json(label);
   });
 }
 
 function createLabelHandler(req, res) {
-  _createLabel(req.user.id, req.body.name, req.body.users).then(label => {
+  Label.createWithStatus({
+    userId: req.user.id,
+    name: req.body.name,
+  }).then(label => {
     res.json(label);
   }).catch(err => {
     res.status(400).send(err.message);
@@ -288,9 +27,8 @@ function createLabelHandler(req, res) {
 }
 
 function updateLabelHandler(req, res) {
-  _updateLabel(req.user.id, req.params.id, {
+  Label.updateWithStatus(req.params.id, req.user.id, {
     name: req.body.name,
-    priority: req.body.priority,
     visibled: req.body.visibled,
   }).then(label => {
     res.json(label);
@@ -298,24 +36,18 @@ function updateLabelHandler(req, res) {
 }
 
 function destroyLabelHandler(req, res) {
-  _destroyLabel(req.user.id, req.params.id).then(() => {
-    res.status(204).send();
+  Label.destroyByUser(req.params.id, req.user.id).then(label => {
+    res.status(204).json(label);
   });
 }
 
 function sortLabelHandler(req, res) {
-  _sortLabel(req.user.id, req.params.id, req.body.priority).then(labels => {
+  Label.sort(req.params.id, req.user.id, req.body.priority).then(labels => {
     res.json(labels);
   });
 }
 
 module.exports = {
-  _indexLabel,
-  _showLabel,
-  _createLabel,
-  _updateLabel,
-  _destroyLabel,
-  _sortLabel,
   indexLabelHandler,
   showLabelHandler,
   createLabelHandler,
--- api/src/models/label.js
+++ api/src/models/label.js
@@ -1,3 +1,23 @@
+function _getMembers(labelId, requests, users) {
+  const members = [];
+  for (let j = 0; j < requests.length; j++) {
+    const request = requests[j];
+    if (labelId === request.labelId) {
+      for (let k = 0; k < users.length; k++) {
+        const user = users[k];
+        if (user.id === request.memberId || user.id === request.userId) {
+          members.push({
+            id: user.id,
+            username: user.username,
+            requestStatus: request.status,
+          });
+        }
+      }
+    }
+  }
+  return members;
+}
+
 module.exports = (sequelize, DataTypes) => {
   const Label = sequelize.define('Label', {
     userId: {
@@ -24,6 +44,251 @@ module.exports = (sequelize, DataTypes) => {
     timestamps: true,
     underscored: true,
   });
+
+  // Public static findAllFromStatus
+  // public static findByIdAndUser
+  // public static createWithStatus
+  // public static updateWithStatus
+  // public static destroyByUser
+  // public static sort
+
+  Label.findAllFromStatus = function (options) {
+    const LabelStatus = sequelize.models.LabelStatus;
+    const Request = sequelize.models.Request;
+    const User = sequelize.models.User;
+
+    return new Promise(resolve => {
+      LabelStatus.findAll(options).then(labelStatuses => {
+        const labelIds = labelStatuses.map(labelStatus => labelStatus.labelId);
+        Promise.all([
+          Label.findAll({
+            where: {id: labelIds},
+          }),
+          Request.findAll({
+            where: {labelId: labelIds},
+          }),
+        ]).then(values => {
+          const labels = values[0];
+          const requests = values[1];
+
+          const userIds = requests.map(request => request.memberId).filter(memberId => (Boolean(options.userId) || memberId !== options.userId));
+          User.findAll({
+            where: {id: userIds},
+          }).then(users => {
+            const labels_ = labelStatuses.map(labelStatus => {
+              let newLabel = {};
+              for (let i = 0; i < labels.length; i++) {
+                const label = labels[i];
+                if (label.id === labelStatus.labelId) {
+                  newLabel = {
+                    id: label.id,
+                    name: label.name,
+                    priority: labelStatus.priority,
+                    visibled: labelStatus.visibled,
+                    createdAt: label.createdAt,
+                    updatedAt: label.updatedAt,
+                    members: _getMembers(label.id, requests, users),
+                  };
+                }
+              }
+              return newLabel;
+            });
+            resolve(labels_);
+          });
+        });
+      });
+    });
+  };
+
+  Label.findByIdAndUser = function (labelId, userId) {
+    const LabelStatus = sequelize.models.LabelStatus;
+    const Request = sequelize.models.Request;
+    const User = sequelize.models.User;
+
+    return new Promise(resolve => {
+      Promise.all([
+        Label.findById(labelId),
+        LabelStatus.findOne({
+          where: {userId, labelId},
+        }),
+        Request.findAll({
+          where: {labelId},
+        }),
+      ]).then(values => {
+        const label = values[0];
+        const labelStatus = values[1];
+        const requests = values[2];
+
+        const userIds = requests.map(request => request.memberId).filter(memberId => (memberId !== userId));
+        User.findAll({
+          where: {id: userIds},
+        }).then(users => {
+          const newLabel = {
+            id: label.id,
+            name: label.name,
+            priority: labelStatus.priority,
+            visibled: labelStatus.visibled,
+            createdAt: labelStatus.createdAt,
+            updatedAt: labelStatus.updatedAt,
+            members: _getMembers(label.id, requests, users),
+          };
+          resolve(newLabel);
+        });
+      });
+    });
+  };
+
+  Label.createWithStatus = function (values) {
+    const LabelStatus = sequelize.models.LabelStatus;
+
+    // UserId, name
+    return new Promise(resolve => {
+      Promise.all([
+        Label.create({
+          userId: values.userId,
+          name: values.name,
+        }),
+        LabelStatus.count({
+          where: {
+            userId: values.userId,
+          },
+        }),
+      ]).then(values_ => {
+        const label = values_[0];
+        const count = values_[1];
+
+        LabelStatus.create({
+          userId: values.userId,
+          labelId: label.id,
+          priority: count,
+          visibled: true,
+        }).then(() => {
+          Label.findByIdAndUser(label.id, values.userId).then(label_ => {
+            resolve(label_);
+          });
+        });
+      });
+    });
+  };
+
+  Label.updateWithStatus = function (labelId, userId, values) {
+    const LabelStatus = sequelize.models.LabelStatus;
+
+    return new Promise(resolve => {
+      Promise.all([
+        Label.update({
+          name: values.name,
+        }, {where: {id: labelId}}),
+        LabelStatus.update({
+          // Priority: values.priority,
+          visibled: values.visibled,
+        }, {where: {userId, labelId}}),
+      ]).then(() => {
+        Label.findByIdAndUser(labelId, userId).then(label => {
+          resolve(label);
+        });
+      });
+    });
+  };
+
+  Label.destroyByUser = function (labelId, userId) {
+    const LabelStatus = sequelize.models.LabelStatus;
+    const Task = sequelize.models.Task;
+    const Request = sequelize.models.Request;
+
+    return new Promise(resolve => {
+      Label.findByIdAndUser(labelId, userId).then(cachedLabel => {
+        LabelStatus.destroy({
+          where: {labelId, userId},
+        }).then(() => {
+          LabelStatus.count({
+            where: {labelId},
+          }).then(count => {
+            if (count === 0) {
+              // If destroy label, remove request and task
+              Label.destroy({
+                where: {id: labelId},
+              }).then(() => {
+                Promise.all([
+                  Request.destroy({
+                    where: {labelId},
+                  }),
+                  Task.destroy({
+                    where: {labelId},
+                  }),
+                ]).then(() => {
+                  resolve(cachedLabel);
+                });
+              });
+            } else {
+              // If don't destroy label, remove request
+              Request.destroy({
+                where: {labelId, memberId: userId},
+              }).then(() => {
+                resolve(cachedLabel);
+              });
+            }
+          });
+        });
+      });
+    });
+  };
+
+  Label.sort = function (labelId, userId, priority) {
+    const LabelStatus = sequelize.models.LabelStatus;
+
+    return new Promise(resolve => {
+      LabelStatus.findOne({
+        where: {userId, labelId},
+      }).then(labelStatus => {
+        if (labelStatus.priority < priority) {
+          LabelStatus.findAll({
+            where: {
+              userId,
+              priority: {
+                $gt: labelStatus.priority,
+                $lte: priority,
+              },
+            },
+          }).then(labelStatuses => {
+            labelStatuses.forEach(labelStatus_ => {
+              labelStatus_.update({priority: labelStatus_.priority - 1});
+            });
+            labelStatus.update({priority}).then(() => {
+              Label.findAllFromStatus({
+                where: {userId},
+                order: [['priority', 'ASC']],
+              }).then(labels => {
+                resolve(labels);
+              });
+            });
+          });
+        } else if (labelStatus.priority > priority) {
+          LabelStatus.findAll({
+            where: {
+              userId,
+              priority: {
+                $gte: priority,
+                $lt: labelStatus.priority,
+              },
+            },
+          }).then(labelStatuses => {
+            labelStatuses.forEach(labelStatus_ => {
+              labelStatus_.update({priority: labelStatus_.priority + 1});
+            });
+            labelStatus.update({priority}).then(() => {
+              Label.findAllFromStatus({
+                where: {userId},
+                order: [['priority', 'ASC']],
+              }).then(labels => {
+                resolve(labels);
+              });
+            });
+          });
+        }
+      });
+    });
+  };
 
   return Label;
 };

6af502d50d0a15e823ae79e601d8298c6e4cfb31
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-07-21 19:22:52 +0900

    Support to update task in task-storyboard

--- web/src/storyboards/task-storyboard/index.tsx
+++ web/src/storyboards/task-storyboard/index.tsx
@@ -4,7 +4,7 @@ import {BackLink} from '../../libs/web-storyboard/back-link';
 
 import Container from '../container';
 
-import {createTask} from '../../action-creators';
+import {createTask, updateTask} from '../../action-creators';
 
 export default class TaskStoryboard extends Container<any, any> {
   public static propTypes = {};
@@ -13,9 +13,31 @@ export default class TaskStoryboard extends Container<any, any> {
     super(props);
 
     this.state = Object.assign({}, this.state, {
+      taskId: null,
       content: '',
-      labelId: (this.state.labels[0] || {}).id,
+      labelId: (this.state.labels[0]) ? this.state.labels[0].id : null,
     });
+  }
+
+  componentDidUpdate(prevProps: any, prevState: any) {
+    if (prevState.taskId !== this.state.selectedTaskId) {
+      const taskId = this.state.selectedTaskId;
+      const task = (taskId) ? this.state.tasks.filter((task: any) => task.id === taskId)[0] : null;
+      if (task) {
+        this.setState({
+          taskId,
+          content: task.content,
+        });
+      }
+    }
+    if (!prevState.labelId) {
+      const labelId = this.state.selectedLabelId || (this.state.labels[0]) ? this.state.labels[0].id : null;
+      this.setState({labelId});
+    }
+  }
+
+  private isUpdate() {
+    return (this.state.taskId !== null);
   }
 
   public handleChangeLabelSelect(event: any) {
@@ -30,12 +52,20 @@ export default class TaskStoryboard extends Container<any, any> {
     });
   }
 
-  public handleClickCreateButton(event: any) {
+  public handleClickCreateOrUpdateButton(event: any) {
     const dispatch = this.props.store.dispatch.bind(this.props.store);
-    createTask(dispatch, {
-      labelId: this.state.labelId,
-      content: this.state.content,
-    }, {accessToken: this.accessToken});
+    const taskId = this.state.taskId;
+    if (this.isUpdate()) {
+      updateTask(dispatch, taskId, {
+        labelId: this.state.labelId,
+        content: this.state.content,
+      }, {accessToken: this.accessToken});
+    } else {
+      createTask(dispatch, {
+        labelId: this.state.labelId,
+        content: this.state.content,
+      }, {accessToken: this.accessToken});
+    }
   }
 
   public render() {
@@ -47,9 +77,9 @@ export default class TaskStoryboard extends Container<any, any> {
           <select onChange={(event) => this.handleChangeLabelSelect(event)}>
             {this.state.labels.map((label: any, index: number) => <option value={label.id} key={index}>{label.name}</option>)}
           </select>
-          <input onChange={(event) => this.handleChangeContentInput(event)}></input>
+          <input value={this.state.content} onChange={(event) => this.handleChangeContentInput(event)}></input>
         </div>
-        <div onClick={(event) => this.handleClickCreateButton(event)}>Create task</div>
+        <div onClick={(event) => this.handleClickCreateOrUpdateButton(event)}>{(this.isUpdate() ? 'Update' : 'Create')}</div>
       </section>
     );
   }

7c8e2df517cd4d26b3e34c5122252ea48275b848
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-07-23 21:34:21 +0900

    Update action creators

--- web/src/action-creators/index.ts
+++ web/src/action-creators/index.ts
@@ -4,6 +4,7 @@ import {
   Task,
   Token,
   User,
+  Member,
 } from '../repositories';
 
 // Utils
@@ -19,9 +20,11 @@ function mapTask(task: any) {
   return {
     id: task.id,
     labelId: task.labelId,
+    text: task.text,
     content: task.content,
     priority: task.priority,
     completed: task.completed,
+    schedule: task.schedule,
     errors: [],
   };
 }
@@ -39,8 +42,8 @@ function mapLabel(label: any) {
 function mapRequest(request: any) {
   return {
     id: request.id,
-    username: request.username,
-    labelId: request.labelId,
+    memberName: request.memberName,
+    labelName: request.labelName,
     status: request.status,
     errors: [],
   };
@@ -53,11 +56,13 @@ export function fetchInitialData(defaults: any, dispatch: (action: any) => void,
       Task.fetch({}, options),
       Label.fetch({}, options),
       Request.fetch({status: ['pending']}, options),
+      Member.fetch(options),
     ]).then((values) => {
       const user: any = values[0];
       const tasks: any = values[1];
       const labels: any = values[2];
       const requests: any = values[3];
+      const members: any = values[4];
 
       const action = {
         type: '__FETCH_INITIAL_DATA',
@@ -66,6 +71,7 @@ export function fetchInitialData(defaults: any, dispatch: (action: any) => void,
         tasks: tasks.map((task: any) => mapTask(task)),
         labels: labels.map((label: any) => mapLabel(label)),
         requests: requests.map((request: any) => mapRequest(request)),
+        members: members.map((member: any) => mapUser(member)),
         selectedTaskId: defaults.selectedTaskId || null,
         selectedLabelId: defaults.selectedLabelId || null,
       };
@@ -246,11 +252,14 @@ export function updateLabelWithRequest(
 ) {
   return new Promise((resolve, reject) => {
     Label.update(id, labelParams, options).then((label: any) => {
-      const action = {
-        type: '__UPDATE_LABEL',
-        label,
-      };
-      dispatch(action);
+      Promise.all(
+        requestParams.memberNames.map((memberName: string) => {
+          return Request.create({
+            labelId: label.id,
+            memberName,
+          }, options);
+        }),
+      });
     });
   });
 }
@@ -282,6 +291,23 @@ export function sortLabel(
       const action = {
         type: '__SORT_LABEL',
         labels,
+      };
+      dispatch(action);
+    });
+  });
+}
+
+export function updateRequest(
+  dispatch: (action: any) => void,
+  id: number,
+  params: {status?: string; },
+  options: any,
+) {
+  return new Promise((resolve, reject) => {
+    Request.update(id, params, options).then((request: any) => {
+      const action = {
+        type: '__UPDATE_REQUEST',
+        request,
       };
       dispatch(action);
     });

0eeb275e14e40c5c898adc349f6f4d580f88aa3a
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-07-29 08:31:36 +0900

    Styling requests-tab-content

--- web/src/storyboards/main-storyboard/requests-tab-content.tsx
+++ web/src/storyboards/main-storyboard/requests-tab-content.tsx
@@ -8,24 +8,28 @@ export class RequestsTabContent extends React.Component<any, any> {
     const actions = this.props.actions;
 
     return (
-      <div>
+      <div className="requests-tab-content">
       {(requests.length) ? (
-        <ul>
+        <ul className="request-list">
           {requests.map((request: any) => {
             return (
-              <li key={request.id}>
-                <strong>{request.labelName}</strong>
-                From {request.memberName}
-                <div>
-                  <div onClick={() => {actions.acceptRequest(request.id);}}>Accept</div>
-                  <div onClick={() => {actions.refuseRequest(request.id);}}>Refuse</div>
+              <li key={request.id} className="request-list--item">
+                <div className="request-list--item--information-container">
+                  <div className="request-list--item--label-name">{request.labelName}</div>
+                  <div className="request-list--item--description">From {request.memberName}</div>
+                </div>
+                <div className="request-list--item--button-container">
+                  <div className="request-list--item--accept-button" onClick={() => {actions.acceptRequest(request.id);}}>Accept</div>
+                  <div className="request-list--item--refuse-button" onClick={() => {actions.refuseRequest(request.id);}}>Refuse</div>
                 </div>
               </li>
             );
           })}
         </ul>
       ) : (
-        <p>No notifications</p>
+        <div className="no-request-content">
+          <p>No notifications</p>
+        </div>
       )}
       </div>
     );

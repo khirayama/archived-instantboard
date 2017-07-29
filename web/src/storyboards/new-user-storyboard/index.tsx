9be9fdbb817e499f3887390328712def167908bd
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-07-29 18:58:28 +0900

    Update new user storyboard user interface

--- web/src/storyboards/new-user-storyboard/index.tsx
+++ web/src/storyboards/new-user-storyboard/index.tsx
@@ -1,7 +1,11 @@
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
+import * as classNames from 'classnames';
 
-import {updateUser} from '../../action-creators';
+import {
+  updateUser,
+  validUser,
+} from '../../action-creators';
 import Container from '../container';
 
 export default class NewUserStoryboard extends Container<any, any> {
@@ -15,7 +19,7 @@ export default class NewUserStoryboard extends Container<any, any> {
     super(props);
 
     this.state = Object.assign(this.state, {
-      username: this.state.user.username || 'khirayama',
+      username: (this.state.user) ? this.state.user.username : '',
     });
   }
 
@@ -23,29 +27,43 @@ export default class NewUserStoryboard extends Container<any, any> {
     this.setState({
       username: event.currentTarget.value,
     });
+
+    const dispatch = this.props.store.dispatch.bind(this.props.store);
+    const username = event.currentTarget.value.trim();
+    validUser(dispatch, {username}, {accessToken: this.accessToken});
   }
 
   public handleClickRegisterButton() {
     const dispatch = this.props.store.dispatch.bind(this.props.store);
-    updateUser(dispatch, {
-      username: this.state.username,
-    }, {accessToken: this.accessToken}).then(() => {
-      this.context.move('/');
-    });
+    const username = this.state.username.trim();
+
+    if (username) {
+      updateUser(dispatch, {username}, {accessToken: this.accessToken}).then(() => {
+        this.context.move('/');
+      });
+    }
   }
 
   public render() {
+    const errors = (this.state.user) ? this.state.user.errors || [] : [];
+
     return (
-      <section className="storyboard">
-        <h1>NewUserStoryboard</h1>
-        <input
-          type="text"
-          value={this.state.username}
-          onChange={(event) => {this.handleChangeUsername(event); }}
-          placeholder="Username"
-        />
-        <p>{(this.state.user.errors.length) ? this.state.user.errors.join(', ') : null}</p>
-        <div onClick={() => {this.handleClickRegisterButton(); }}>Start!</div>
+      <section className="new-user-storyboard storyboard">
+        <section className="new-user-storyboard--content">
+          <p>
+            Please put your screen name.<br/>
+            Get it used with share your tasks.
+          </p>
+          <input
+            type="text"
+            autoFocus
+            value={this.state.username}
+            onChange={(event) => {this.handleChangeUsername(event); }}
+            placeholder="My screen name"
+          />
+          <p className="error-messages">{errors.join(', ')}</p>
+          <div className={classNames("start-app-button", {"start-app-button__disabled": !this.state.username.trim() || errors.length})} onClick={() => {this.handleClickRegisterButton(); }}>Start app</div>
+        </section>
       </section>
     );
   }
